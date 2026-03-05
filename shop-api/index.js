8abd84d652a1e13310a9c6fffb4829a09ae5419b
annieversaryyyyy <kduzkdkuzkdkd@gmail.com>
2026-03-05 14:25:31 +0600

    fix: move root route outside run() for railway

--- shop-api/index.js
+++ shop-api/index.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const products = require("./app/products");
 const categories = require("./app/categories");
-const users = require('./app/users')
+const users = require("./app/users");
 const config = require("./config");
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -15,15 +15,19 @@ app.use(cors());
 
 app.use("/products", products);
 app.use("/categories", categories);
-app.use("/users", users)
+app.use("/users", users);
+
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
 const run = async () => {
   try {
     await mongoose.connect(config.mongo.db, config.mongo.options);
     console.log("MongoDB connected");
 
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
     });
 
     process.on("SIGINT", async () => {
@@ -39,5 +43,3 @@ const run = async () => {
 };
 
 run();
-
-
