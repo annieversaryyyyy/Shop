491c794a03a69681712b00e6215addaead2e8cb0
annieversaryyyyy <kduzkdkuzkdkd@gmail.com>
2026-03-05 18:53:02 +0600

    fix: update index.js

--- shop-api/index.js
+++ shop-api/index.js
@@ -5,13 +5,12 @@ const mongoose = require("mongoose");
 const products = require("./app/products");
 const categories = require("./app/categories");
 const users = require("./app/users");
-const config = require("./config");
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/products", products);
 app.use("/categories", categories);
@@ -21,21 +20,14 @@ app.get("/", (req, res) => {
   res.send("API working");
 });
 
-const run = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected");
+const PORT = process.env.PORT;
 
-    const PORT = process.env.PORT || 8080;
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("MongoDB connected");
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-
-  } catch (e) {
-    console.error("Startup error:", e);
-    process.exit(1);
-  }
-};
-
-run();
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server started on port " + PORT);
+  });
+}).catch(err => {
+  console.error(err);
+});
