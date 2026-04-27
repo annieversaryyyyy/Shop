2fbd62cce5c07d613a22be3a096ed45a562429b5
annieversaryyyyy <kduzkdkuzkdkd@gmail.com>
2026-04-27 15:09:53 +0600

    change url

--- frontend/src/shared/config/config.js
+++ frontend/src/shared/config/config.js
@@ -1,7 +1,7 @@
-export let apiUrl = "http://localhost:8000";
+export let apiUrl = "https://shop-8sc7.onrender.com";
 export const googleClientId =
   "163796225030-mihi8qo1v96g5f2ifph79ou2r3li7tkf.apps.googleusercontent.com";
 
 if (process.env.REACT_APP_ENV === "test") {
-  apiUrl = "http://localhost:8010";
+  apiUrl = "https://shop-8sc7.onrender.com";
 }
